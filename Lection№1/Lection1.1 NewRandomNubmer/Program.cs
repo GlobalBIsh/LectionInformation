72a298c47b7bf4b1fc63e02c77d5ac614210e879
Nikita <gamula.n@mail.ru>
2021-12-10 18:12:37 +0300

    add LectionAllinformation

---
+++ Lection№1/Lection1.1 NewRandomNubmer/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = new Random().Next(1,20); //тут мы выбрали случайно число от 1 до 20 
+
+Console.WriteLine (numberA);//Здесь мы показываем какое случайное число нам может выпасть
+
+int numberB = 30;//new Random().Next(1,50); //тут мы так же  выбираем случайно число от 1 до 50
+
+Console.WriteLine (numberB);//Здесь мы показываем какое случайное число нам может выпасть
+//Но так же мы можем ввести 1 из значений на постоянной  основе.
+
+
+int result = numberB + numberA; 
+
+Console.WriteLine(result);
+
+
+
---
+++ Lection№1/Lection1.3 PeretaskivanieGir/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 111;
+int b = 2;
+int d = 4;
+int c = 7;
+int e = 6;
+int n = 9;
+int g = 56;
+
+int max = a; 
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (d > max) max = d;
+if (c > max) max = c;
+if (e > max) max = e;
+if (n > max) max = n;
+if (g > max) max = g;
+
+Console.Write("max = ");
+Console.WriteLine (max);
